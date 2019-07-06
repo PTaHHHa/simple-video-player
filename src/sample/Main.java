a440b33bac257d714e893faa771bb1586f916fab
PTaHHHa <36362252+PTaHHHa@users.noreply.github.com>
2019-07-06 22:14:57 +0300

    very simple video player

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +14,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("VideoPlayer");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setFullScreen(true);
+//        primaryStage.setFullScreen(KeyCombination.keyCombination("F"));
+        //primaryStage.setFullScreenExitHint("Аахахха,еблан");
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.keyCombination("ESC"));
         primaryStage.show();
     }
 
